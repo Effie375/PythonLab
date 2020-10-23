81bc4a74549789beabc92f746bcbe9db4bf25d45
Effie375 <71607343+Effie375@users.noreply.github.com>
2020-10-24 01:11:52 +0300

    add the exercise 3

--- src/Lab_02_Exercise_3a.py
+++ src/Lab_02_Exercise_3a.py
@@ -1,11 +1,17 @@
-# Askisi 3a
+# Exercise 3a
 
 import datetime
 
-name = input("Dwse to onoma sou: ")
-age = int(input("Dwse tin ilikia sou: "))
-year = datetime.datetime.today().year
+def calc_year(age):
+    now_year = datetime.datetime.today().year
+    return now_year + 100 - age 
 
-x = 100 - age
-y = year + x
-print("O/H ", name, "to etos ", y, " tha einai 100 xronon.")
+
+my_name = input("Enter your name: ")
+try:
+    my_age = int(input("Enter your age: "))
+except:
+    print("Please enter an integer!")
+else:
+    end_year = calc_year(my_age)
+    print(my_name, " in ", end_year, " will be 100 years old.")
--- src/Lab_02_Exercise_3b.py
+++ src/Lab_02_Exercise_3b.py
@@ -1,12 +1,9 @@
-# Askisi 3b
+# Exercise 3b
 
-import datetime
-name = input("Dwse to onoma sou: ")
-age = int(input("Dwse tin ilikia sou: "))
-year = datetime.datetime.today().year
+my_name = input("Enter your name: ")
+my_age = int(input("Enter your age: "))
+now_year = 2020
 
-z = year - age
-e = 100 + z
-print("O/H ", name, "to etos ", e, "tha einai 100 xronon.")
+end_year = 100 + now_year - my_age
 
-
+print(my_name, " in ", end_year, " will be 100 years old.")
