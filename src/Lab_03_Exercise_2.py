5785e217fbf11da921b526eb4dcdff4a85188409
Efstratia Kazi <71607343+Effie375@users.noreply.github.com>
2020-10-30 14:47:58 +0200

    Create Lab_03_Exercise_2.py

---
+++ src/Lab_03_Exercise_2.py
@@ -0,0 +1,12 @@
+#Exercise 2
+
+number = input("Dwse arithmo: ")
+
+if number.isdigit():
+    number = int(number)
+    if (number >= 5) and (number <= 10):
+        print("perases to mathima!")
+    else:
+        print("kopikes, prospathise ksana!")
+else:
+    print("mi egkiros arithmos")
