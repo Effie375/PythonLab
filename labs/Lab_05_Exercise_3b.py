2a4e8f7cb47c9cbef10be4da3b775556c333509e
Effie375 <71607343+Effie375@users.noreply.github.com>
2020-11-14 18:43:22 +0200

    add new exercises

---
+++ labs/Lab_05_Exercise_3b.py
@@ -0,0 +1,27 @@
+#list_according_to=["b","c","d","f","g","j","k","l","m","n","p","q","r","s","t","v","w","x","z"]
+#list_of_vowels=["a","e","h","y","u","i","o"]
+
+list_according_to = "bcdfgjklmnpqrstvwxz"
+list_of_vowels = "aehyuio"
+
+i = 0
+vow = 0
+agr = 0
+bal = 0
+
+word = input("Enter a word: ")
+
+while i < len(word):
+    if word[i] in list_of_vowels:
+        vow += 1
+    elif word[i] in list_according_to:
+        agr += 1
+    else:
+        bal += 1
+    i += 1
+    
+    
+print("The vowels are:", vow)
+print("The agreements are:",agr)
+print("Τhe symbols and the blanks are", bal)
+
