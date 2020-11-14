5565709a6b95825fa5750b5194b9407553d038eb
Effie375 <71607343+Effie375@users.noreply.github.com>
2020-11-14 23:34:17 +0200

    Create Lecture_04_Example_4.py

---
+++ lectures/Lecture_04_Example_4.py
@@ -0,0 +1,18 @@
+a = int(input("a: "))
+x = int(input("x: "))
+y = float(input("y: "))
+
+if a == 10:
+    x = x % 2
+    y = y**2
+elif a == 3:
+    x = x * 2
+    y -= 1
+elif a == 5:
+    x = x + 4
+    y += 7
+else:
+    x -= 3
+    y += 1
+
+print("\nx: %d , y: %.1f" %(x,y))
