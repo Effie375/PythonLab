d6ad813e3adef1aaa4a56ba20442ea0498674852
Ioannis Christodoulakis <48016428+YodaCh96@users.noreply.github.com>
2020-11-14 11:13:58 +0100

    Create Lab_02_Exercise_1a.py

---
+++ labs/Lab_02_Exercise_1a.py
@@ -0,0 +1,8 @@
+name = input("Dwse to onoma sou: ")
+proodos = float(input("Dwse vathmo proodou: "))
+grapta = float(input("Dwse vathmo graptwn: "))
+
+telikos_vathmos = round(0.2 * proodos + 0.8 * grapta, 1)
+
+print("Onoma foititi:", name)
+print("Telikos vathmos:", telikos_vathmos)
