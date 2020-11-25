3ea3b2beb26e8e696bfb63c0c1c00853a20de0c0
Effie375 <71607343+Effie375@users.noreply.github.com>
2020-11-26 01:19:02 +0200

    add exercise

--- Laboratories/Lab_04_Exercise_1.py
+++ Laboratories/Lab_04_Exercise_1.py
@@ -1,35 +1,20 @@
-"""
-Να γραφεί πρόγραμμα το οποίο:
-* Θα διαβάζει το αποτέλεσμα 2 ζαριών.
-* Θα ελέγχει αν ο χρήστης εισήγαγε σωστά νούμερα (1-6), αλλιώς θα 
-εμφανίζει μήνυμα λάθους και θα τερματίζει.
-* Αν τα ζάρια ήταν τεσσάρια, θα τυπώνει «Ντορτια».
-* Αν τα ζάρια ήταν 1 και 2, θα τυπώνει «Ασσόδυο».
-* Αν τα ζάρια ήταν διπλά, θα τυπώνει «Διπλές» και το αποτέλεσμα.
-* Αλλιώς θα τυπώνει το αποτέλεσμα της ζαριάς.
-"""
+first_dice = input("Enter the first dice: ")
+second_dice = input("Enter the second dice: ")
 
-# Το πρόγραμμα δεν έχει τελειώσει ακόμα!
-
-first_dice = int(input("Enter the first dice: "))
-second_dice = int(input("Enter the second dice: "))
-
-
-if (first_dice >= 1 and first_dice <= 6) and (second_dice >= 1 and second_dice <= 6):
-    if first_dice and second_dice == 4:
-    print("Dortia.")
-    elif (first_dice and second_dice) == (1 and 2):
-        print("Assodo.")
-    elif first_dice == second_dice:
-        print("Double.")
+if first_dice.isdigit() and second_dice.isdigit():
+    first_dice = int(first_dice)
+    second_dice = int(second_dice)
+    if (first_dice >= 1 and first_dice <= 6) and (second_dice >= 1 and second_dice <= 6):
+        if ((first_dice == 4) and (second_dice == 4)):
+            print("Dortia.")
+        elif ((first_dice == 1) and (second_dice == 2)) or ((first_dice == 2) and (second_dice == 1)) :
+            print("Assodyo.")
+        elif first_dice == second_dice:
+            print("Double.")
+        else:
+            print("First dice %d." % first_dice)
+            print("Second dice %d." % second_dice)
     else:
-    print("First dice %d." % first_dice)
-    print("Second dice %d." % second_dice)
-else:
-    print(ERROR!!!!)
-    
-
-
-
-
-
+        print("ERROR!!!!")
+else: 
+    print("ERROR!!")
