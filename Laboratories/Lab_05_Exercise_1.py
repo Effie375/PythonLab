8af7dfddd320b4d71138e85d4d5b66004f980dbf
Effie375 <71607343+Effie375@users.noreply.github.com>
2020-11-25 23:19:30 +0200

    add more exercises

--- Laboratories/Lab_05_Exercise_1.py
+++ Laboratories/Lab_05_Exercise_1.py
@@ -2,20 +2,51 @@
 Να γραφεί πρόγραμμα το οποίο θα διαβάζει 10 ακεραίους αριθμούς και
 θα τους αποθηκεύει σε μία λίστα. Έπειτα θα υπολογίζει και θα
 εμφανίζει:
-1) Το άθροισμα των στοιχείων της λίστας.
 2) Το πλήθος των άρτιων στοιχείων της λίστας.
 3) Το πλήθος των περιττών στοιχείων της λίστας.
 4) Τη διαφορά του μέγιστου από το ελάχιστο στοιχείο της λίστας.
 """
-
-# Το πρόγραμμα δεν έχει τελειώσει ακόμα!
-
+odd = 0
+even = 0
 i = 0 
-lista = []
+sum = 0
+
+list = []
 
 while i < 5:
     num = int(input("Enter a number: "))
-    lista.append(num)
-    x = (lista[i] % 2) == 0
+    list.append(num)
+    sum += num 
+    if num % 2 == 0:
+        even += 1
+    else:
+        odd += 1
     i += 1
-    print(x)
+
+print("The sum of the numbers is:", sum)
+print("The number of odd numbers is:", odd)
+print("The number of even numbers is:",even)
+
+i = 0
+max = 0
+min = 0
+
+while i < 5:
+    if list[i] > list[max]:
+        max = i
+    elif list[i] < list[min] :
+        min = i
+    i += 1
+
+sub = list[max] - list[min]
+print("Τhe difference between the %d and the %d number is %d." % (list[max], list[min], sub))
+
+
+
+
+
+
+ 
+
+
+    
--- Laboratories/Lab_06_Exercise_1.py
+++ Laboratories/Lab_06_Exercise_1.py
@@ -5,4 +5,21 @@
 χρήση της μεθόδου count().
 """
 
-# Το πρόγραμμα δεν έχει τελειώσει ακόμα!
+num = 1
+numbers = []
+
+while num != 0:
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+
+i = 0
+counter = 0
+
+key = int(input("Enter the number you are looking for: "))
+
+while i < len(numbers):
+    if key == numbers[i]:
+        counter += 1
+    i += 1
+
+print("Τhe number %d has been entered %d times" % (key, counter))
