03d4b213c791b075e85006f286825c3ec5429b19
Ioannis Christodoulakis <48016428+YodaCh96@users.noreply.github.com>
2020-11-26 16:38:00 +0100

    Update def_odd_even_a.py

--- tests/def_odd_even_a.py
+++ tests/def_odd_even_a.py
@@ -1,3 +1,7 @@
+# Python program to check if the input number is odd or even.
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number.
+
 def odd_even():
     num = int(input("Enter a number: "))
     if num % 2 == 0:
@@ -6,4 +10,5 @@ def odd_even():
         print(num, "is odd.")
 
 
-odd_even() 
+if __name__ == "__main__":    
+    odd_even()
