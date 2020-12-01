050d69d2ce3f15b6e51553756b91f221838c1ab4
Ioannis Christodoulakis <48016428+YodaCh96@users.noreply.github.com>
2020-12-01 09:43:16 +0100

    Update Lab_02_Exercise_1a.py

--- Laboratories/Lab_02_Exercise_1a.py
+++ Laboratories/Lab_02_Exercise_1a.py
@@ -6,7 +6,7 @@ def _main():
     telikos_vathmos = round(0.2 * proodos + 0.8 * grapta, 1)
 
     print("Όνομα φοιτητή: %s" % name)
-    print("Τελικός βαθμός: %.2f" % telikos_vathmos)
+    print("Τελικός βαθμός: %.1f" % telikos_vathmos)
 
 
 if __name__ == "__main__":
