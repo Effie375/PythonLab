9fd623b314f198986880312bb55fe03620aba2f7
Ioannis Christodoulakis <48016428+YodaCh96@users.noreply.github.com>
2020-12-01 09:33:53 +0100

    Update Lab_02_Exercise_1a.py

--- Laboratories/Lab_02_Exercise_1a.py
+++ Laboratories/Lab_02_Exercise_1a.py
@@ -1,8 +1,13 @@
-name = input("Dwse to onoma sou: ")
-proodos = float(input("Dwse vathmo proodou: "))
-grapta = float(input("Dwse vathmo graptwn: "))
+def _main():
+    name = input("Δώσε το όνομα σου: ")
+    proodos = float(input("Δώσε βαθμό προόδου: "))
+    grapta = float(input("Δώσε βαθμό γραπτών: "))
 
-telikos_vathmos = round(0.2 * proodos + 0.8 * grapta, 1)
+    telikos_vathmos = round(0.2 * proodos + 0.8 * grapta, 1)
 
-print("Onoma foititi:", name)
-print("Telikos vathmos:", telikos_vathmos)
+    print("Όνομα φοιτητή: %s" % name)
+    print("Τελικός βαθμός: %.2f" % telikos_vathmos)
+
+
+if __name__ == "__main__":
+    _main()
