ccb98022e87fdf6fe359f7c37e208eb0d50c4934
Ioannis Christodoulakis <48016428+YodaCh96@users.noreply.github.com>
2020-12-01 09:55:30 +0100

    Update Lab_02_Exercise_2a.py

--- Laboratories/Lab_02_Exercise_2a.py
+++ Laboratories/Lab_02_Exercise_2a.py
@@ -1,12 +1,17 @@
-miktes_apodoxes = float(input("Dwse tis miktes apodoxes: "))
+def _main():
+    miktes_apodoxes = float(input("Δώσε τις μεικτές αποδοχές: "))
 
-asfaleia = round(miktes_apodoxes * 0.03, 2)
-foros = round(miktes_apodoxes * 0.05, 2)
-loipes_kratiseis = round(miktes_apodoxes * 0.09, 2)
-kathares_apodoxes = round(miktes_apodoxes - asfaleia - foros - loipes_kratiseis, 2)
-    
-print("Miktes apodoxes:", miktes_apodoxes)
-print("Asfaleia 3%:", asfaleia)
-print("Foros 5%:", foros)
-print("Loipes kratiseis 9%:", loipes_kratiseis)
-print("Kathares apodoxes:", kathares_apodoxes)
+    asfaleia = miktes_apodoxes * 0.03
+    foros = miktes_apodoxes * 0.05
+    loipes_kratiseis = miktes_apodoxes * 0.09
+    kathares_apodoxes = miktes_apodoxes - asfaleia - foros - loipes_kratiseis
+        
+    print("Μεικτές αποδοχές: %.2f" % miktes_apodoxes)
+    print("Ασφάλεια 3%%: %.2f" % asfaleia)
+    print("Φόρος 5%%: %.2f" % foros)
+    print("Λοιπές κρατήσεις 9%%: %.2f" % loipes_kratiseis)
+    print("Καθαρές αποδοχές: %.2f" % kathares_apodoxes)
+
+
+if __name__ == "__main__":
+    _main()
