892565f1881451b111a05a321970b3c567192412
Effie375 <71607343+Effie375@users.noreply.github.com>
2020-12-13 16:21:12 +0200

    add example 1 from lab 9

--- Laboratories/Lab_09_Example_1.py
+++ Laboratories/Lab_09_Example_1.py
@@ -1,1 +1,15 @@
-# To πρόγραμμα δεν έχει τελειώσει.
+list = [9,3,7,5,2] 
+
+key = int(input("Δώσε στοιχείο που αναζητάς: "))
+
+found = False
+thesi = 0
+
+for i in list:
+    if i == key:
+        print("Το %d βρίσκεται στη %d θέση της λίστας." % (key, thesi))
+        found = True
+    thesi += 1 
+
+if found == False:
+    print("Το %d δε βρίσκεται στη λίστα." % key)
