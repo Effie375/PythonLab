e44d8c11c6b99132a1ef9a992ad76159e13e8396
Effie375 <71607343+Effie375@users.noreply.github.com>
2020-12-13 16:30:04 +0200

    add example 3 from lab 9

--- Laboratories/Lab_09_Example_3.py
+++ Laboratories/Lab_09_Example_3.py
@@ -1,1 +1,16 @@
-# To πρόγραμμα δεν έχει τελειώσει.
+list = []
+
+for i in range(5):
+    num = int(input("Δώσε στοιχείο: "))
+    list.append(num)
+
+print("H μη ταξινομημένη λίστα είναι: %s" % list)
+
+for i in range(1,5):
+    for j in range(4,i-1,-1):
+        if list[j-1] > list[j]:
+            temp = list[j-1]
+            list[j-1] = list[j]
+            list[j] = temp
+
+print("H ταξινομημένη λίστα είναι: %s" % list)
