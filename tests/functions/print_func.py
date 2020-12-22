354e28bb57573ba03f4a4bc58e3a26238f84e5c0
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2020-12-22 11:18:02 +0100

    testing the E501 of flake8

--- tests/functions/print_func.py
+++ tests/functions/print_func.py
@@ -4,7 +4,7 @@
 By default python's print() function ends with a newline. A programmer with C/C++ background may wonder how to print without newline.
 
 Python's print() function comes with a parameter called 'end'. By default, the value of this parameter is '\n', i.e. the new line character. You can end a print statement with any character/string using this parameter.
-"""
+""" # noqa: E501
 
 # ends the output with a <space>
 print("Welcome to" , end = ' ')
