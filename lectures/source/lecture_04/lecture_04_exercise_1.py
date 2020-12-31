7208e9a1f4f34c7bd77a45f241a0b998a87e8f3c
Effie375 <71607343+Effie375@users.noreply.github.com>
2020-12-31 13:59:31 +0200

    change in lecture 4

--- lectures/source/lecture_04/lecture_04_exercise_1.py
+++ lectures/source/lecture_04/lecture_04_exercise_1.py
@@ -1,11 +1,11 @@
 plithos = 0
 
-num = float(input(("Δώσε αριθμό: "))
+num = float(input("Δώσε αριθμό: "))
 
 while num != 0:
     if num > 0:
         plithos += 1
-        num = float(input(("Δώσε αριθμό: "))
+        num = float(input("Δώσε αριθμό: "))
 
 if plithos > 0:
     print("Θετικοί:", plithos)
