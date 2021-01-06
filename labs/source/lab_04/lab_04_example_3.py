6ee563019488259ddf1910b1e0c035a299c52418
Effie375 <71607343+Effie375@users.noreply.github.com>
2021-01-06 23:38:28 +0200

    add lab-files

--- labs/source/lab_04/lab_04_example_3.py
+++ labs/source/lab_04/lab_04_example_3.py
@@ -1,15 +1,15 @@
-# Αρχικοποίηση μεταβλητών
+# Αρχικοποιούμε τις μεταβλητές
 i = 0
 synolo = 0
 
 while i < 10:
     number = input("Δώσε αριθμό: ")
-    # Έλεγχος ορθότητας
+    # Κάνουμε τον έλεγχο ορθότητας
     while not number.isdigit():
         number = input("Είπα δώσε αριθμό: ")
     number = int(number)
     synolo = synolo + number
     i = i + 1
 
-# Εκτύπωση αποτελέσματος
+# Εκτυπώνουμε το αποτέλεσμα
 print("Το σύνολο είναι:", synolo)
