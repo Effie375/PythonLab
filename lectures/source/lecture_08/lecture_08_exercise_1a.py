a327e49ecd3703005afa6768eddac26ef6a19481
Effie375 <71607343+Effie375@users.noreply.github.com>
2021-01-17 00:29:41 +0200

    fixed flake8

--- lectures/source/lecture_08/lecture_08_exercise_1a.py
+++ lectures/source/lecture_08/lecture_08_exercise_1a.py
@@ -6,10 +6,10 @@ for i in range(MATHITES):
     list.append([])
     sum = 0
     for j in range(TRIMHNA):
-        vathmos = int(input(f"Δώσε βαθμό {j+1}oυ τριμήνου: "))
+        vathmos = int(input(f"Δώσε βαθμό {j+1}oυ τριμήνου: ").strip())
         sum += vathmos
         list[i].append(vathmos)
-    list[i].append(sum // TRIMHNA)    
+    list[i].append(sum // TRIMHNA)
 
-for vathmos in range(MATHITES):
-    print(f"O μέσος όρος του {vathmos+1}ου μαθητή είναι {list[vathmos][TRIMHNA]}.")
+for g in range(MATHITES):
+    print(f"O μέσος όρος του {g+1}ου μαθητή είναι {list[g][TRIMHNA]}.")
