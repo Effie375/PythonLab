80c34ae08c583f35f107835241d237591b595da2
Effie375 <71607343+Effie375@users.noreply.github.com>
2021-01-29 10:58:57 +0200

    Update lecture 05

--- lectures/source/lecture_05/lecture_05_exercise_4.py
+++ lectures/source/lecture_05/lecture_05_exercise_4.py
@@ -1,21 +1,33 @@
+# Αρχικοποίηση των μεταβλητών
 MAX_STUDENTS = 20
-student_list = []
-grades_list = []
+studentList = []
+gradesList = []
 maxIndex = 0
 i = 0
 
+# Για όσο το i είναι μικρότερο από το MAX_STUDENTS
 while i < MAX_STUDENTS:
+    # Ζητάμε από το χρήστη να δώσει το όνομά του
     student = input("Δώσε το όνομά σου: ")
-    student_list.append(student)
+    # Απoθηκεύουμε το όνομα στη λίστα studentList
+    studentList.append(student)
+    # Ζητάμε από το χρήστη να δώσει το βαθμό του και το μετατρέπουμε σε ακέραιο
     grad = int(input("Δώσε τον βαθμό σου: "))
-    grades_list.append(grad)
+    # Απoθηκεύουμε το βαθμό στη λίστα gradesList
+    gradesList.append(grad)
+    # Αυξάνουμε το i κατά 1
     i += 1
 
+# Αρχικοποίηση μεταβλητής
 i = 0
 
-while i < len(grades_list):
-    if grades_list[i] > grades_list[maxIndex]:
+# Για όσο το i είναι μικρότερο από το μήκος της λίστας
+while i < len(gradesList):
+    if gradesList[i] > gradesList[maxIndex]:
+        # Εκχωρούμε στο maxIndex το στιγμιαίο i
         maxIndex = i
+    # Αυξάνουμε το i κατά 1
     i += 1
 
-print("%s κέρδισε με βαθμό %d." % (student_list[maxIndex], grades_list[maxIndex]))
+# Eκτύπωση όνοματος και βαθμού του νικητή
+print("%s κέρδισε με βαθμό %d." % (studentList[maxIndex], gradesList[maxIndex]))
