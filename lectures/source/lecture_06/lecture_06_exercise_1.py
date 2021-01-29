3475bdfec3dec6d703cea3afdf57409108985392
Effie375 <71607343+Effie375@users.noreply.github.com>
2021-01-29 13:54:54 +0200

    fixed flake8

--- lectures/source/lecture_06/lecture_06_exercise_1.py
+++ lectures/source/lecture_06/lecture_06_exercise_1.py
@@ -7,6 +7,6 @@ for i in range(MAX_ELEMENTS):
     # Ζητάμε από το χρήστη να δώσει αριθμό, το μετατρέπουμε σε ακέραιο και το αποθηκεύουμε στη λίστα
     lista.append(int(input("Δώσε αριθμό: ")))
 
-for i in range(MAX_ELEMENTS -1, -1, -1):
+for i in range(MAX_ELEMENTS - 1, -1, -1):
     # Εκτύπωση κάθε φορά το στιγμιαίο στοιχείο
     print(lista[i])
