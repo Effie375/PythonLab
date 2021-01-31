8062cfebe3ce27b94ce9febd7a9fcaaccf37c1a2
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-01-31 20:10:26 +0100

    update the PythonLab

--- Python/labs/source/lab_02/lab_02_exercise_3.py
+++ Python/labs/source/lab_02/lab_02_exercise_3.py
@@ -1,7 +1,5 @@
-import datetime
-
-# Ζητάμε από το χρήστη να δώσει το όνομα του
-onoma = input("Ποιο είναι το όνομα σου: ")
+# Ζητάμε από το χρήστη να δώσει το όνομά του
+onoma = input("Πώς σε λένε; ")
 
 # Ζητάμε από το χρήστη να δώσει την ηλικία του
 ilikia = input("Ποια είναι η ηλικία σου: ")
@@ -9,22 +7,14 @@ ilikia = input("Ποια είναι η ηλικία σου: ")
 # Μετατροπή από str σε int
 ilikia = int(ilikia)
 
-# Η βιβλιοθήκη datetime βρίσκει το τωρινό έτος
-twrinoEtos = datetime.datetime.today().year
+# Τωρινό έτος
+twrinoEtos = 2021
 
 # Υπολογισμός έτους γέννησης
 etosGennhshs = twrinoEtos - ilikia
 
 # Υπολογισμός χρονιάς που θα γίνει 100
 ekatoXronwn = etosGennhshs + 100
-
-# Εάν το όνομα τελειώνει σε 'ς' ή 's'
-if (onoma[-1] == 's') or (onoma[-1] == 'ς'):
-    # Σβήσε το τελευταίο γράμμα
-    onoma = onoma[:-1]
-
-# Κάνε το πρώτο γράμμα κεφαλαίο
-onoma = onoma[0].upper() + onoma[1:]
 
 # Εκτύπωση αποτελέσματος
 print("%s θα γίνεις 100 το έτος %d." % (onoma, ekatoXronwn))
